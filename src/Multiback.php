3f32d2e1c16bc166fbba32e41a9daacd0b9c398f
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2022-10-30 01:22:58 +0100

    entrypoint setup

--- src/Multiback.php
+++ src/Multiback.php
@@ -1,6 +1,42 @@
 <?php
 
-namespace Multiback;
+namespace Emmaakachukwu\Multiback;
 
 class Multiback
-{}
+{
+    /**
+     * Should backup database
+     * 
+     * @var bool
+     */
+    protected bool $backupDb;
+
+    /**
+     * Should backup files and directories specified in all .gitignore files
+     * 
+     * @var bool
+     */
+    protected bool $backupGitIgnoredList;
+
+    /**
+     * List of files and directories to backup
+     * 
+     * @var array
+     */
+    protected array $backupList;
+
+    /**
+     * List of files and directories to ignore during backup
+     * 
+     * @var array
+     */
+    protected array $ignoredList;
+
+    public function __construct()
+    {
+        $this->init();
+    }
+    
+    protected function init()
+    {}
+}
